309ad68b6c626ba44ad23668fa3c99f067780631
Aayush9029 <aayushpokharel36@gmail.com>
2022-10-15 16:22:51 -0400

    Initial Commit

---
+++ Elkk/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Elkk
+//
+//  Created by Aayush Pokharel on 2022-10-15.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
